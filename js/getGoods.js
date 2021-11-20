ea778c629a739c76ce3f789bc66b4e31a8eade78
Somov Eugene <somov.eugene.2012@gmail.com>
2021-11-20 16:36:34 +0300

    Day2: Retrieving data from the server

---
+++ js/getGoods.js
@@ -0,0 +1,26 @@
+const getGoods = () => {
+  const links = document.querySelectorAll(".navigation-link");
+
+  const getData = () => {
+    const URL = "/db/db.json";
+    // const URL = "https://test-73003-default-rtdb.firebaseio.com/db.json";
+
+    fetch(URL)
+      .then((res) => res.json())
+      .then((data) => {
+        // console.log(data);
+        localStorage.setItem("goods", JSON.stringify(data));
+      });
+  };
+
+  links.forEach(link => {
+    link.addEventListener("click", (event) => {
+      event.preventDefault();
+      getData();
+    });
+  });
+
+  // const goods = JSON.parse(localStorage.getItem("goods"));
+};
+
+getGoods();
