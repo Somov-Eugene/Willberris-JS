76b405d9b25eff6cd713b6fa3d43b802df5e54af
Somov Eugene <somov.eugene.2012@gmail.com>
2021-11-20 15:40:56 +0300

    Day1: Event handling

---
+++ js/cart.js
@@ -0,0 +1,10 @@
+const cart = () => {
+  const buttonСart = document.querySelector(".button-cart");
+  const modalСart = document.getElementById("modal-cart");
+  const modalCartClose = modalСart.querySelector(".modal-close");
+
+  buttonСart.addEventListener("click", () => modalСart.style.display = "flex");
+  modalCartClose.addEventListener("click", () => modalСart.style.display = "");
+};
+
+cart();
