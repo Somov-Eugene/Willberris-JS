5f82a4e7bc5744dfcc0281996d59148eaaa9f3b5
Somov Eugene <somov.eugene.2012@gmail.com>
2021-11-22 21:59:46 +0300

    Added script for smooth scroll to top pages

---
+++ src/modules/smoothScroll.js
@@ -0,0 +1,11 @@
+const smoothScroll = () => {
+  const links = document.querySelectorAll(".scroll-link");
+  links.forEach((link) => {
+    link.addEventListener("click", (e) => {
+      e.preventDefault();
+      window.scrollTo({ top: 0, behavior: "smooth" });
+    });
+  });
+};
+
+export default smoothScroll;
